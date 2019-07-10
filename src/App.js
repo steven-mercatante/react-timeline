527e305b3a72162c7c0b067d3c6d2c458958539b
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-10 18:08:09 -0400

    start adding Marker

--- src/App.js
+++ src/App.js
@@ -7,19 +7,18 @@ import "./App.css";
 function App() {
   const events = [
     { date: "7/10/19", body: "Began prototyping React Timeline" },
-
-    { tweet: "1006202911632904197" },
-    {
-      date: "1/1/2019",
-      body: "You can include images",
-      image: {
-        src:
-          "https://images.unsplash.com/photo-1562696482-77619dec0ae7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
-        alt: "trees"
-      }
-    },
-    { body: "**Markdown** is *supported*", date: "2/1/2019" },
-    { youtube: "8ktONdU_wXU" }
+    // { tweet: "1006202911632904197" },
+    // {
+    //   date: "1/1/2019",
+    //   body: "You can include images",
+    //   image: {
+    //     src:
+    //       "https://images.unsplash.com/photo-1562696482-77619dec0ae7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
+    //     alt: "trees"
+    //   }
+    // },
+    { body: "**Markdown** is *supported*", date: "2/1/2019" }
+    // { youtube: "8ktONdU_wXU" }
   ];
   return (
     <div>
--- src/components/Event.js
+++ src/components/Event.js
@@ -26,6 +26,7 @@ const Container = styled.div`
 `;
 
 const Content = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: flex-end;
@@ -48,6 +49,18 @@ const ImageContainer = styled.div`
     object-fit: cover;
     overflow: hidden;
   }
+`;
+
+const Marker = styled.span`
+  background-color: #1a1a1c;
+  border: 3px solid #950f44;
+  border-radius: 50%;
+  position: absolute;
+  top: calc(50% - 10px);
+  right: -40px;
+  width: 20px;
+  height: 20px;
+  z-index: 100;
 `;
 
 export default function Event({ event }) {
@@ -78,6 +91,8 @@ export default function Event({ event }) {
         {tweet && <TweetEmbed id={tweet} />}
 
         {youtube && <YouTubeEmbed id={youtube} />}
+
+        <Marker />
       </Content>
     </Container>
   );
