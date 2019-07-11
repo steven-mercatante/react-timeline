648916fb6ca1e9acd44bed1d2d691eb4e14d732f
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-11 10:02:47 -0400

    add inline flag to Timestamp

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import "./App.css";
 function App() {
   const events = [
     { date: "7/10/19", body: "Began prototyping React Timeline" },
+    { date: "July - September 2019", body: "Began prototyping React Timeline" },
     { date: "7/10/19", tweet: "1006202911632904197" },
     { date: "7/10/19", youtube: "8ktONdU_wXU" },
     {
-      date: "1/1/2019",
+      date: "Sometime in 2018",
       body: "You can include images",
       image: {
         src:
           "https://images.unsplash.com/photo-1562696482-77619dec0ae7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
         alt: "trees"
+      }
+    },
+    {
+      date: "1/1/2019",
+      image: {
+        src:
+          "https://images.unsplash.com/photo-1562734041-a2d56f060a44?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80",
+        alt: "fireworks"
       }
     },
     { date: "7/10/19", body: "**Markdown** is *supported*", date: "2/1/2019" }
--- src/components/Event.js
+++ src/components/Event.js
@@ -4,7 +4,6 @@ import ReactMarkdown from "react-markdown";
 import TweetEmbed from "./TweetEmbed";
 import YouTubeEmbed from "./YoutubeEmbed";
 import Image from "./Image";
-import EventMarker from "./EventMarker";
 
 const Container = styled.div`
   // border: 1px solid red;
@@ -12,23 +11,15 @@ const Container = styled.div`
   justify-content: flex-end;
   width: 50%;
   align-self: flex-start;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
 
   :nth-child(1) {
-    margin-top: 10px;
+    margin-top: 20px;
   }
 
   :nth-child(even) {
     align-self: flex-end;
     justify-content: flex-start;
-
-    .content {
-      align-items: flex-start;
-
-      .marker {
-        left: -40px;
-      }
-    }
   }
 `;
 
@@ -45,7 +36,11 @@ const Content = styled.div`
   padding: 10px;
   margin: 0 27px;
   opacity: ${({ isVisible }) => (isVisible ? 1 : 0.25)};
-  transition: opacity 0.5s ease-in;
+  transition: opacity 0.25s ease-in;
+
+  ${Container}:nth-child(even) & {
+    align-items: flex-start;
+  }
 `;
 
 const Timestamp = styled.time`
@@ -55,11 +50,27 @@ const Timestamp = styled.time`
   border-radius: 4px;
   font-weight: 500;
   font-size: .85rem;
-  position: absolute;
+  position: ${({ inline }) => (inline === true ? "static" : "absolute")};
   top: calc(50% - 10px);
   ${({ placement }) => placement === 0 && `right: -110px`}
   ${({ placement }) => placement === 1 && `left: -110px`}
   z-index: 100;
+`;
+
+const Marker = styled.span`
+  background-color: #ee18b6;
+  border: 2px solid #ee18b6;
+  border-radius: 50%;
+  position: absolute;
+  top: calc(50% - 10px);
+  right: -40px;
+  width: 20px;
+  height: 20px;
+  z-index: 100;
+
+  ${Container}:nth-child(even) & {
+    left: -40px;
+  }
 `;
 
 export default function Event({ event, placement }) {
@@ -94,12 +105,14 @@ export default function Event({ event, placement }) {
   return (
     <Container className="event" ref={eventRef}>
       <Content className="content" isVisible={isVisible}>
-        <Timestamp placement={placement}>{date}</Timestamp>
+        <Timestamp placement={placement} inline={true}>
+          {date}
+        </Timestamp>
         <ReactMarkdown source={body} />
         {image && <Image {...image} />}
         {tweet && <TweetEmbed id={tweet} />}
         {youtube && <YouTubeEmbed id={youtube} />}
-        <EventMarker className="marker" />
+        <Marker className="marker" />
       </Content>
     </Container>
   );
