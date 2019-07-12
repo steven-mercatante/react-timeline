35fd98d8fc67071f02a2d8d2e667c124ce13950d
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-11 20:27:29 -0400

    extract EventTimestamp to its own component

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useRef, useState } from "react";
-import styled from "styled-components";
 import ReactMarkdown from "react-markdown";
 import TweetEmbed from "./TweetEmbed";
 import YouTubeEmbed from "./YoutubeEmbed";
@@ -7,28 +6,7 @@ import Image from "./Image";
 import EventContainer from "./EventContainer";
 import EventContent from "./EventContent";
 import EventMarker from "./EventMarker";
-
-// TODO: avoid using !important in CSS
-
-const Timestamp = styled.time`
-  background-color: #ec24b5;
-  position: ${({ inline }) => (inline ? "static" : "absolute")};
-  top: calc(50% - 10px);
-  color: #fff
-  padding: 4px;
-  margin-bottom: 4px;
-  border-radius: 4px;
-  font-weight: 500;
-  font-size: .85rem;
-
-  ${EventContainer}:nth-child(odd) & {
-    left: calc(50% + 30px);
-  }
-  
-  ${EventContainer}:nth-child(even) & {
-    right: calc(50% + 30px);
-  }
-`;
+import EventTimestamp from "./EventTimestamp";
 
 export default function Event({ event, alternateEvents, inlineDate }) {
   // console.log("Event.render()");
@@ -75,10 +53,10 @@ export default function Event({ event, alternateEvents, inlineDate }) {
       isVisible={isVisible}
       alternateEvents={alternateEvents}
     >
-      {!inlineDate && <Timestamp>{date}</Timestamp>}
+      {!inlineDate && <EventTimestamp>{date}</EventTimestamp>}
       <EventMarker className="marker" alternateEvents={alternateEvents} />
       <EventContent className="content" alternateEvents={alternateEvents}>
-        {inlineDate && <Timestamp inline={true}>{date}</Timestamp>}
+        {inlineDate && <EventTimestamp inline={true}>{date}</EventTimestamp>}
         <ReactMarkdown source={body} />
         {image && <Image {...image} />}
         {tweetId && <TweetEmbed id={tweetId} />}
