82f3c9f936d1927500acd3c0d824bd6b74da8764
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-13 11:13:30 -0400

    build ES version

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,34 @@ import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
 
 export default [
+  // CommonJS
   {
     input: "./src/index.js",
     output: {
       file: "bundle.js",
       format: "cjs",
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM"
+      }
+    },
+    external: ["react", "react-dom", "styled-components"],
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      resolve(),
+      commonjs()
+    ]
+  },
+
+  // ES
+  // CommonJS
+  {
+    input: "./src/index.js",
+    output: {
+      file: "es/bundle.js",
+      format: "es",
       globals: {
         react: "React",
         "react-dom": "ReactDOM"
