24dc00dc93d8a64bba327432aa44e0f6c8000583
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-13 11:55:01 -0400

    import pkg in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,14 @@
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
 
 export default [
   {
     input: "src/index.js",
     output: [
-      { file: "dist/bundle.cjs.js", format: "cjs" },
-      { file: "dist/bundle.esm.js", format: "es" }
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" }
     ],
     external: ["react", "react-dom", "styled-components"],
     plugins: [
