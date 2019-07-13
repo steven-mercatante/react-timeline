2c3f6780eb44d3ea9a71108948329f098d5e4af2
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-12 22:46:24 -0400

    add basic skeleton view to TwitterNode

--- src/components/TwitterNode.js
+++ src/components/TwitterNode.js
@@ -2,8 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
+// TODO: show loading indicator while Tweet loads?
 const Container = styled.div`
+  min-width: 300px;
   width: 100%;
+  min-height: 120px;
+  background-color: white;
 `;
 
 export default function TwitterNode({ event }) {
