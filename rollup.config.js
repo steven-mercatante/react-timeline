17442905e9b43c0d2a498f6f40bc97b212da8ed4
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-13 12:02:41 -0400

    use rollup-plugin-peer-deps-external

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
 import pkg from "./package.json";
 
 export default [
@@ -10,8 +11,8 @@ export default [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" }
     ],
-    external: ["react", "react-dom", "styled-components"],
     plugins: [
+      external(),
       babel({
         exclude: "node_modules/**"
       }),
