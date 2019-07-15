8ce822a36ec27b8bb4c52393f7e30ac28d88a64d
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-15 13:25:22 -0400

    stub basic timeline

--- examples/images-timeline/src/App.js
+++ examples/images-timeline/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Timeline from "@merc/react-timeline";
+
+const events = [{ type: "text", date: "7/15/19", text: "hi!" }];
 
 function App() {
-  return <div className="App">sup</div>;
+  return (
+    <div>
+      <Timeline events={events} />
+    </div>
+  );
 }
 
 export default App;
