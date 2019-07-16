9b6e3085f75cb4d0fa013844136f011011865b43
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-16 16:00:30 -0400

    allow passing text to YouTubeNode

--- src/components/YouTubeNode.js
+++ src/components/YouTubeNode.js
@@ -1,14 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import Markdown from "markdown-to-jsx";
 
 const Container = styled.div`
   position: relative;
   overflow: hidden;
-  padding-top: 56.25%;
-
-  // display: flex;
-  // flex-direction: row;
-  // height: 100%;
+  padding-bottom: 56.25%;
 `;
 
 const RespIframe = styled.iframe`
@@ -18,23 +15,24 @@ const RespIframe = styled.iframe`
   width: 100%;
   height: 100%;
   border: 0;
-
-  // display: 1 1 auto;
 `;
 
 export default function YouTubeNode({ event }) {
-  const { id, name } = event;
+  const { id, name, text } = event;
   return (
-    <Container className="youtube-container">
-      <RespIframe
-        title={name}
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${id}`}
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      />
-    </Container>
+    <div>
+      {text && <Markdown>{text}</Markdown>}
+      <Container className="youtube-container">
+        <RespIframe
+          title={name}
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${id}`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </Container>
+    </div>
   );
 }
