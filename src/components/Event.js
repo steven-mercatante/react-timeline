2e1eb6064be8efafb562d25652ebbe7dc0b72cf6
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-20 14:44:52 -0400

    merge EventInner into Event

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,8 +1,46 @@
 import React from "react";
 import EventContainer from "./EventContainer";
 import useIntersectionObserver from "../hooks/useIntersectionObserver";
-import EventInner from "./EventInner";
-import kebabCase from 'lodash.kebabcase'
+import EventTimestamp from "./EventTimestamp";
+import EventMarker from "./EventMarker";
+import EventContent from "./EventContent";
+import styled from "styled-components";
+import kebabCase from "lodash.kebabcase";
+
+const FlexColumn = styled.div`
+  // border: 1px solid cyan;
+  display: flex;
+  flex-direction: column;
+  flex-basis: ${({ flexBasis }) => flexBasis};
+  justify-content: center;
+  align-items: flex-end;
+
+  &.alternate-events,
+  &.alternate-events-inline-date {
+    flex-basis: 50%;
+
+    ${EventContainer}:nth-child(even) & {
+      align-items: flex-start;
+    }
+  }
+
+  &.alternate-events-inline-date {
+    margin-bottom: 10px;
+  }
+
+  &.inline-events {}
+    flex-basis: 100px;
+    justify-content: center;
+    align-items: flex-end;
+  }
+
+  &.inline-events-inline-date {
+    margin-bottom: 10px;
+    flex-basis: 0;
+    justify-content: center;
+    align-items: flex-end;
+  }
+`;
 
 export default function Event({ event, children, isCompact, opts }) {
   // TODO: can the intersection observer opts be passed via param so user can customize?
@@ -12,7 +50,10 @@ export default function Event({ event, children, isCompact, opts }) {
     threshold: 0.5
   });
 
-  const classes = ["event", event.type.toLowerCase(), kebabCase(opts.layout)];
+  const inlineTimestamp = opts.layout.toLowerCase().includes("inlinedate");
+  const kebabLayout = kebabCase(opts.layout);
+
+  const classes = ["event", event.type.toLowerCase(), kebabLayout];
   if (event.opts && event.opts.cssClass) {
     classes.push(event.opts.cssClass);
   }
@@ -25,14 +66,23 @@ export default function Event({ event, children, isCompact, opts }) {
       animationsEnabled={opts.animationsEnabled}
       layout={opts.layout}
     >
-      <EventInner
-        event={event}
-        isCompact={isCompact}
-        isVisible={isVisible}
-        opts={opts}
+      <FlexColumn className={`col-1 ${kebabLayout}`} flexBasis="50%">
+        {!isCompact && !inlineTimestamp && (
+          <EventTimestamp>{event.date}</EventTimestamp>
+        )}
+      </FlexColumn>
+
+      <EventMarker layout={kebabLayout} />
+
+      <EventContent
+        className="node-content"
+        width={event.type.toLowerCase() === "youtube" ? "500px" : null}
       >
+        {(isCompact || inlineTimestamp) && (
+          <EventTimestamp inline={true}>{event.date}</EventTimestamp>
+        )}
         {children}
-      </EventInner>
+      </EventContent>
     </EventContainer>
   );
 }
