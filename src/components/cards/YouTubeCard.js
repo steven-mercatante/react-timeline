241387c90bb2ff89b4800c59ad0d495789315a96
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-23 14:31:09 -0400

    support children in YouTubeEvent & YouTubeCard

--- src/components/cards/YouTubeCard.js
+++ src/components/cards/YouTubeCard.js
@@ -3,11 +3,12 @@ import ConditionalMarkdown from "../atoms/ConditionalMarkdown";
 import Card from "./Card";
 import YouTubeAtom from "../atoms/YouTubeAtom";
 
-export default function YouTubeCard({ date, id, name, text }) {
+export default function YouTubeCard({ date, id, name, text, children }) {
   return (
     <Card date={date}>
       <ConditionalMarkdown>{text}</ConditionalMarkdown>
       <YouTubeAtom id={id} name={name} />
+      {children}
     </Card>
   );
 }
--- src/components/events/YouTubeEvent.js
+++ src/components/events/YouTubeEvent.js
@@ -3,11 +3,13 @@ import Event from "./Event";
 import YouTubeCard from "../cards/YouTubeCard";
 
 export default function YouTubeEvent(props) {
-  const { date, id, name, text, marker } = props;
+  const { date, id, name, text, marker, children } = props;
 
   return (
     <Event date={date} marker={marker}>
-      <YouTubeCard date={date} id={id} name={name} text={text} />
+      <YouTubeCard date={date} id={id} name={name} text={text}>
+        {children}
+      </YouTubeCard>
     </Event>
   );
 }
