5b88bd277f17a7c9885abbdb0234b58bca715b46
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-23 13:17:33 -0400

    use YouTubeAtom in YouTubeCard

--- src/components/YouTubeCard.js
+++ src/components/YouTubeCard.js
@@ -2,37 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import ConditionalMarkdown from "./ConditionalMarkdown";
 import CardWrapper from "./CardWrapper";
-
-const Container = styled.div`
-  position: relative;
-  overflow: hidden;
-  padding-bottom: 56.25%;
-`;
-
-const RespIframe = styled.iframe`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border: 0;
-`;
+import YouTubeAtom from "./YouTubeAtom";
 
 export default function YouTubeCard({ date, id, name, text }) {
   return (
     <CardWrapper date={date}>
       <ConditionalMarkdown>{text}</ConditionalMarkdown>
-      <Container className="youtube-container">
-        <RespIframe
-          title={name}
-          width="560"
-          height="315"
-          src={`https://www.youtube.com/embed/${id}`}
-          frameBorder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-      </Container>
+      <YouTubeAtom id={id} name={name} />
     </CardWrapper>
   );
 }
