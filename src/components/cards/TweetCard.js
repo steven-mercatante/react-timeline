07ae01d0272c44a193516bb8246d128f10859af6
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-23 14:30:14 -0400

    support children in TweetEvent & TweetCard

--- src/components/cards/TweetCard.js
+++ src/components/cards/TweetCard.js
@@ -3,11 +3,12 @@ import ConditionalMarkdown from "../atoms/ConditionalMarkdown";
 import Card from "./Card";
 import TweetAtom from "../atoms/TweetAtom";
 
-export default function TweetCard({ date, id, text }) {
+export default function TweetCard({ date, id, text, children }) {
   return (
     <Card date={date}>
       <ConditionalMarkdown>{text}</ConditionalMarkdown>
       <TweetAtom id={id} />
+      {children}
     </Card>
   );
 }
--- src/components/events/TweetEvent.js
+++ src/components/events/TweetEvent.js
@@ -3,11 +3,13 @@ import Event from "./Event";
 import TweetCard from "../cards/TweetCard";
 
 export default function TweetEvent(props) {
-  const { date, id, text, marker } = props;
+  const { date, id, text, marker, children } = props;
 
   return (
     <Event date={date} marker={marker}>
-      <TweetCard date={date} id={id} text={text} />
+      <TweetCard date={date} id={id} text={text}>
+        {children}
+      </TweetCard>
     </Event>
   );
 }
