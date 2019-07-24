2c357d292d1a06ce273d5e16978c5339c9aa7c6a
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-24 19:51:30 -0400

    tweak button padding

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,21 +2,21 @@ import styled from "styled-components";
 
 const Button = styled.button(props => {
   const defaults = {
-    display: 'inline-block',
+    display: "inline-block",
     fontSize: "14px",
     backgroundColor: "rebeccapurple",
     borderRadius: "4px",
-    padding: "4px",
+    padding: "6px",
     color: "#fff",
     margin: "10px 5px 5px 0",
     border: "none",
-    textDecoration: 'none',
-    cursor: 'pointer'
-  }
+    textDecoration: "none",
+    cursor: "pointer"
+  };
 
   const style = { ...defaults, ...props.theme.button };
 
   return style;
-})
+});
 
 export default Button;
