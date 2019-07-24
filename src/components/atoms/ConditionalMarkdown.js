54b94d936e25e8d517bda1d46ebd850974788256
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-24 10:24:44 -0400

    allow passing className to ConditionalMarkdown

--- src/components/atoms/ConditionalMarkdown.js
+++ src/components/atoms/ConditionalMarkdown.js
@@ -5,26 +5,27 @@ import styled from "styled-components";
 
 const Container = styled.div(props => {
   const defaults = {
-    marginBottom: '10px'
-  }
+    marginBottom: "10px"
+  };
 
   const style = { ...defaults, ...props.theme.conditionalMarkdown };
 
   return style;
-})
+});
 
-export default function ConditionalMarkdown({ children }) {
-  if (! children) {
-    return null
+export default function ConditionalMarkdown({ className, children }) {
+  if (!children) {
+    return null;
   }
 
   return (
-    <Container>
+    <Container className={className}>
       <Markdown>{children}</Markdown>
     </Container>
   );
 }
 
 ConditionalMarkdown.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.string.isRequired
 };
