2ba87da91f1aa1f47304f5663642e7ac54ddc1b9
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-24 19:29:57 -0400

    add loading state to TweetAtom

--- src/components/atoms/TweetAtom.js
+++ src/components/atoms/TweetAtom.js
@@ -1,24 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
 // TODO: show loading indicator while Tweet loads?
 
-const Tweet = styled.div`
-  .twitter-tweet {
-    width: 100% !important;
-  }
-`;
+const Loading = styled.div(props => {
+  const defaults = {
+    width: "100%",
+    height: "200px",
+    backgroundColor: "#fff"
+  };
+
+  const style = { ...defaults, ...props.theme.twitterAtomLoading };
+
+  return style;
+});
+
+const Tweet = styled.div(props => {
+  const defaults = {
+    ".twitter-tweet": {
+      width: "100% !important"
+    }
+  };
+
+  const style = { ...defaults, ...props.theme.twitterAtom };
+
+  return style;
+});
 
 export default function TweetAtom({ id }) {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    window.twttr.widgets.createTweet(
-      id,
-      document.querySelector(`.tweet[data-id="${id}"]`)
-    );
+    window.twttr.widgets
+      .createTweet(id, document.querySelector(`.tweet[data-id="${id}"]`))
+      .then(resp => {
+        setLoading(false);
+      });
   }, [id]);
 
-  return <Tweet className="tweet" data-id={id} />;
+  return (
+    <React.Fragment>
+      {loading && <Loading className="tweet-loading" />}
+      <Tweet className="tweet" data-id={id} />
+    </React.Fragment>
+  );
 }
 
 TweetAtom.propTypes = {
