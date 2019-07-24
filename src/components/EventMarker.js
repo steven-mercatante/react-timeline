937a93ed12bba3f715a6507932c44d227f15114f
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-24 10:42:22 -0400

    tweak EventMarker default style

--- src/components/EventMarker.js
+++ src/components/EventMarker.js
@@ -3,19 +3,18 @@ import styled from "styled-components";
 
 const Marker = styled.span(props => {
   const defaults = {
-    backgroundColor: '#fff',
-    border: '2px solid black',
-    borderRadius: '50%',
-    width: '20px',
-    height: '20px',
+    backgroundColor: "#fff",
+    border: "2px solid #EB2DB4",
+    borderRadius: "50%",
+    width: "20px",
+    height: "20px",
     zIndex: 100
-  }
+  };
 
   const style = { ...defaults, ...props.theme.marker };
 
   return style;
-})
-
+});
 
 export default function EventMarker() {
   return <Marker className="marker" />;
