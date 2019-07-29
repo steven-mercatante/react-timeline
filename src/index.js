05bc13a627c0432387ebeb4dd0d33a7650e14809
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-29 18:16:20 -0400

    export ImageAtom

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Events from './components/Events';
 import Timeline from './components/Timeline';
 import themes from './themes';
 
+import ImageAtom from './components/atoms/ImageAtom';
 import TextAtom from './components/atoms/TextAtom';
 import TweetAtom from './components/atoms/TweetAtom';
 import YouTubeAtom from './components/atoms/YouTubeAtom';
@@ -23,6 +24,7 @@ export {
   EventMarker,
   Events,
   Card,
+  ImageAtom,
   ImageEvent,
   TextEvent,
   TextAtom,
