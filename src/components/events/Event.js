e384c8ab08932829a650e381fa73bc9d659aa8f4
Steven Mercatante <steven.mercatante@gmail.com>
2019-07-29 19:53:09 -0400

    add card prop type to Event

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -172,4 +172,5 @@ Event.propTypes = {
   marker: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  card: PropTypes.func,
 };
