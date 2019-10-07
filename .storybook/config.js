ea07037749e63cee6ac8ca37c497d384cfc7dec7
Steven Mercatante <steven.mercatante@gmail.com>
2019-10-07 16:34:14 -0400

    tweak storybook style

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure } from '@storybook/react';
+import './storybook.css';
 
 configure(require.context('../src', true, /\.stories\.js$/), module);
 configure(require.context('../src', true, /\.stories\.mdx$/), module);
