5a4cc69f329733a24f222cbdee98822224b08701
Steven Mercatante <steven.mercatante@gmail.com>
2019-10-22 19:00:10 -0400

    update UrlButtonContainer

--- src/components/UrlButton.js
+++ src/components/UrlButton.js
@@ -2,7 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const UrlButtonContainer = styled.a(props => props.theme.urlButton);
+const UrlButtonContainer = styled.a`
+  display: inline-block;
+  text-decoration: none;
+  font-size: ${props => props.theme.urlButton.fontSize};
+  background-color: ${props => props.theme.urlButton.backgroundColor};
+  border: ${props => props.theme.urlButton.border};
+  border-radius: ${props => props.theme.urlButton.borderRadius};
+  padding: ${props => props.theme.urlButton.padding};
+  margin: ${props => props.theme.urlButton.margin};
+  color: ${props => props.theme.urlButton.color};
+`;
 
 export default function UrlButton({ href, target, children }) {
   const extraProps = {};
