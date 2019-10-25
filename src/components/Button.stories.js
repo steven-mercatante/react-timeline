91a8859f41ef1de56fd9a23a204075528b68ecf3
Steven Mercatante <steven.mercatante@gmail.com>
2019-10-24 22:13:11 -0400

    add ThemeProvider to broken stories

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import Button from './Button';
+import { ThemeProvider } from 'styled-components';
+import themes from '../themes';
 
 export default {
   title: 'Controls/Button',
   component: Button,
 };
 
-export const button = () => <Button>I'm just a regular button</Button>;
+export const button = () => (
+  <ThemeProvider theme={themes.default}>
+    <Button>I'm just a regular button</Button>
+  </ThemeProvider>
+);
 
 export const withOnClickHandler = () => (
-  <Button
-    onClick={() =>
-      alert(
-        "I'm just a regular button. Use me like you would any other button."
-      )
-    }
-  >
-    Click me for an alert
-  </Button>
+  <ThemeProvider theme={themes.default}>
+    <Button
+      onClick={() =>
+        alert(
+          "I'm just a regular button. Use me like you would any other button."
+        )
+      }
+    >
+      Click me for an alert
+    </Button>
+  </ThemeProvider>
 );
 
 withOnClickHandler.story = {
